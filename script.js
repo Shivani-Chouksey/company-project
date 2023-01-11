c384366260f59716d4fabfb67db399b79fe9fe4e
Shivani-Chouksey <122255940+Shivani-Chouksey@users.noreply.github.com>
2023-01-11 21:11:48 +0530

    key value animation

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var tl=gsap.timeline({
       trigger:" #first",
       start:"top top",
    pin:true,
-   scrub:true
+   scrub:true,
+   end: "+=" + (window.innerHeight * 3)
 },
 })
 
@@ -91,3 +92,142 @@ tl.to("#pimg",{
     delay:.4,
     
  },"c")
+
+tl.to(".key-section-middle-content",{
+   top:"0%",
+   ease:Power1,
+   delay:.5
+},"d")
+tl.to("#pimg",{
+   top:"-20%",
+    ease:Power1,
+    delay:.5,
+    
+ },"d")
+tl.to(".lp-key-data-items ",{
+   top:"0%",
+   ease:Power1,
+   delay:1,
+   scale:.9
+})
+
+tl.to("#key-first-screen-content-wrapper",{
+   filter: "blur(8px)",
+   opacity:0,
+   ease:Power1,
+   delay:-.5
+})
+tl.to("#l-width",{
+   height:"100%",
+   ease:Power1,
+   delay:.5
+},"e")
+tl.to("#key-video",{
+   top:"0%",
+   ease:Power1,
+   delay:.5
+},"e")
+tl.to(".key-data-content1",{
+   y:-280,
+   ease:Power0,
+   delay:.5
+},"e")
+tl.to(".key-data-content2",{
+   y:-135,
+   ease:Power0,
+   delay:.5
+},"e")
+
+tl.to("#l-width",{
+   opacity:0,
+   ease:Power1,
+
+})
+tl.to("#l-width",{
+   height:"0%",
+   ease:Power1,
+})
+
+tl.to("#l-width",{
+   opacity:1,
+   height:"100%",
+   ease:Power1,
+   delay:-.5
+},"f")
+tl.to("#key-video2",{
+   top:"0%",
+   ease:Power1,
+   delay:-.5
+},"f")
+tl.to(".key-data-content2",{
+   y:-280,
+   ease:Power0,
+   delay:-.5
+},"f")
+tl.to(".key-data-content3",{
+   y:-135,
+   ease:Power0,
+   delay:-.5
+},"f")
+tl.to("#l-width",{
+   opacity:0,
+   ease:Power1,
+
+})
+tl.to("#l-width",{
+   height:"0%",
+   ease:Power1,
+})
+
+tl.to("#l-width",{
+   opacity:1,
+   height:"100%",
+   ease:Power1,
+   delay:-.5
+},"g")
+tl.to("#key-video3",{
+   top:"0%",
+   ease:Power1,
+   delay:-.5
+},"g")
+tl.to(".key-data-content3",{
+   y:-280,
+   ease:Power0,
+   delay:-.5
+},"G")
+tl.to(".key-data-content4",{
+   y:-135,
+   ease:Power0,
+   delay:-.5
+},"G")
+tl.to("#l-width",{
+   opacity:0,
+   ease:Power1,
+
+})
+tl.to("#l-width",{
+   height:"0%",
+   ease:Power1,
+})
+
+tl.to("#l-width",{
+   opacity:1,
+   height:"100%",
+   ease:Power1,
+   delay:-.5
+},"h")
+tl.to("#key-img",{
+   top:"0%",
+   ease:Power1,
+   delay:-.5
+},"h")
+tl.to(".key-data-content4",{
+   y:-270,
+   ease:Power0,
+   delay:-.5
+},"h")
+tl.to(".key-data-content5",{
+   y:-135,
+   ease:Power0,
+   delay:-.5
+},"h")
